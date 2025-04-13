088b10735500b44bffc33fe0ce8a64abe60b3c7f
VishritVivek <vivshi654@yahoo.co.in>
2025-04-13 22:01:46 +0530

    Delete MAIN.py

--- MAIN.py
+++
@@ -1,6 +0,0 @@
-a=float(input("whats your first number?"))
-b=float(input("whats your second number?"))
-print("ADDITION ",a+b)
-print("SUBTRACTION ",a-b)
-print("MULTIPLICATION ",a*b)
-print("DIVISION ",a/b)
